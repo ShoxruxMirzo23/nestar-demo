69b6b6ee7097b4fd6e3d415606b9d9553c9fce12
ShoxruxMirzo23 <shohrukh_2019@mail.ru>
2024-06-19 13:16:57 +0500

    feat: develop mobile version

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,25 @@ import TopAgents from "@/libs/components/homepage/TopAgents";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 
 const Home: NextPage = () => {
-  return (
-    <Stack className={"home-page"}>
-      <TrendProperties />
-      <PopularProperties />
-      <Advertisement />
-      <TopProperties />
-      <TopAgents />
-    </Stack>
-  );
+  // DEVICE: MOBILE vs PC
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>HomePage Mobile</Stack>;
+  } else {
+    return (
+      <Stack className={"home-page"}>
+        <TrendProperties />
+        <PopularProperties />
+        <Advertisement />
+        <TopProperties />
+        <TopAgents />
+      </Stack>
+    );
+  }
 };
 
 export default withLayoutMain(Home);
